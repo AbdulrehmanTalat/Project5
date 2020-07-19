9b25b6d577c0ee9b5232f20839ede4da719e1845
AbdulrehmanTalat <66178386+AbdulrehmanTalat@users.noreply.github.com>
2020-07-19 15:34:46 +0500

    image changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import style from '../CSS/Header.module.css';
 import {Carousel, Button} from 'react-bootstrap';
+import slide02 from '../../Images/slider-1.jpg';
+import slide01 from '../../Images/cloud-service-slider.jpg';
+import slide03 from '../../Images/slider-2.jpg';
 export const Header = () => {
     return (
         <div>
             <Carousel>
                 <Carousel.Item>
-                    <img src="https://rapidcompute.com/wp-content/uploads/2019/04/cloud-service-slider.jpg" alt=" "/> 
+                    <img src={slide01} alt=" "/> 
                     <Carousel.Caption>
                         <h1 className={style.txt_color}>
                             Defining
@@ -23,7 +26,7 @@ export const Header = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src="https://rapidcompute.com/wp-content/uploads/2019/04/slider-1.jpg" alt=" "/>
+                    <img src={slide02} alt=" "/>
                     <Carousel.Caption>
                         <h1 className={style.txt_color}>
                             Secure Cloud
@@ -31,7 +34,7 @@ export const Header = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src="https://rapidcompute.com/wp-content/uploads/2019/04/slider-2.jpg" alt=" "/>
+                    <img src={slide03} alt=" "/>
                     <Carousel.Caption>
                         <h1 className={style.txt_color}>
                             The New
